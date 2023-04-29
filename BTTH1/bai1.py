9f6421dbdeea8d8f9d6229077f436068526e7825
lqh403 <lequochungsnk@gmai.com>
2023-04-29 16:08:51 +0700

    add new

--- BTTH1/bai1.py
+++ BTTH1/bai1.py
@@ -1,7 +1,7 @@
 #1. Nhập 1 số nguyên từ bàn phím. Kiểm tra xem số vừa nhập là số chẵn hay số lẻ.
 
 
-a = int(input())
+a = int(input("Mời nhập số: "))
 
 if a % 2 == 0:
     print(f"Số {a} vừa nhập là số chẵn ")
---
+++ BTTH1/bai2.py
@@ -0,0 +1,6 @@
+# 2-	Nhập 1 số nguyên từ bàn phím. Chỉ rõ số vừa nhập có bao nhiêu chữ số
+
+
+a = input("Mời nhập số: ")
+
+print(f" Số {a} có {len(a)} chữ số")
