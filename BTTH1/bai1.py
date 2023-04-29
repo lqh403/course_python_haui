4c3e2cff4fd06a662269549e693823cc43d71952
lqh403 <lequochungsnk@gmai.com>
2023-04-29 15:53:23 +0700

    Add file Le Quoc Hung

---
+++ BTTH1/bai1.py
@@ -0,0 +1,10 @@
+#1. Nhập 1 số nguyên từ bàn phím. Kiểm tra xem số vừa nhập là số chẵn hay số lẻ.
+
+
+a = int(input())
+
+if a % 2 == 0:
+    print(f"Số {a} vừa nhập là số chẵn ")
+else:
+    print(f"Số {a} vừa nhập là số lẻ ")
+        
